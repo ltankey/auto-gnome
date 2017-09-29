1c3b6935ec5b31c1627d09dd7c26f1be2cd51a04
Chris Gough <christopher.d.gough@gmail.com>
2017-09-30 00:38:40 +1000

    tweak app.py to run

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from github import Github
-
+import json
+import sys
 
 app = Flask(__name__)
 
@@ -8,14 +9,17 @@ app = Flask(__name__)
 def index():
     if request.method == 'GET':
         return "OK", 200
+        gh = Github()
+        print(gh, file=sys.stdout)
     elif request.method == 'POST':
-        print(request.headers.get('X-GitHub-Event'))
+        print(request.headers.get('X-GitHub-Event'), file=sys.stdout)
         # ensure the callback is coming from github
         if request.headers.get('X-GitHub-Event') == "ping":
             return json.dumps({'msg': 'Hi!'})
 
         payload=json.loads(request.data)
         #print payload
+
         return json.dumps({'msg': 'thanks for that'})
         #if request.headers.get('X-GitHub-Event') != "push":
         #    return json.dumps({'msg': "wrong event type"})
@@ -23,7 +27,7 @@ def index():
         # no password ATM, only work with public repos
         # TODO: pull credentials from the environment
         gh = Github()
-        print(gh)
+        print(gh, file=sys.stdout)
     else:
         return "not a supported method", 400
 if __name__ == '__main__':
