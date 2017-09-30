ef9d1507bb454cb4e5bf497c5e425001576bc69c
Chris Gough <christopher.d.gough@gmail.com>
2017-09-30 19:26:14 +1000

    change test.sh

--- src/test.sh
+++ src/test.sh
@@ -3,4 +3,4 @@
 #DEV_ENDPOINT=https://4ntcp396gj.execute-api.us-east-1.amazonaws.com/dev
 DEV_ENDPOINT=127.0.0.1:5000
 
-curl -H "Content-Type: application/json" -X POST -d '{"foo":"xyz","bar":"abc"}' $DEV_ENDPOINT
+curl -H "Content-Type: application/json" -X POST -d '{"foo":"xyz","bar":"abc", "contents_url":"https://api.github.com/repos/bugflow/gh-ticket-gnome/contents/{+path}"}' $DEV_ENDPOINT
