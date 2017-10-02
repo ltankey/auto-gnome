b52894c865bee4a76cde00ad5e55b086747c4c49
Chris Gough <christopher.d.gough@gmail.com>
2017-10-02 19:53:56 +1100

    ~50% coverageof gh.py

--- src/gh.py
+++ src/gh.py
@@ -8,8 +8,20 @@ import local_secrets
 GITHUB_USER = local_secrets.GITHUB_USER
 GITHUB_PSX = local_secrets.GITHUB_PSX
 
+def repo_from_callback(callback):
+    full_name = callback.payload()['repository']['full_name']
+    return Repo(full_name)
+
 
 class Milestone:
+    """
+    Wrapper of pygithub.Milestone.Milestone
+    with cache and convenience methods.
+    """
+    def __init__(self, repo, milestone):
+        self._repo = repo
+        self._milestone = milestone
+
     def open_tickets(self):
         return ()
 
@@ -21,11 +33,59 @@ class Issue:
         pass
 
 
-# make sure this is lazy
 class Repo:
+    def __init__(self, repo_name):
+        self.name = repo_name
+        gh = Github(GITHUB_USER, GITHUB_PSX)
+        self._repo = gh.get_repo(repo_name)
+        self._milestones = None # lazy
+
     def ensure_milestone_exists(self, milestone_name, date=None):
-        pass
+        if not self.milestone_exists(milestone_name):
+            self.create_milestone(milestone_name, date=date)
+
     def ensure_milestone_has_due_date(self, milestone_name, due_date):
+        milestone = self.get_milestone(milestone_name)
+        found_due_date = milestone.get_due_date()
+        # FIXME: use propper date comparisons (not strings)
+        if found_due_date != due_date:
+            milestone.set_due_date(due_date)
+
+    def milestone_exists(self, milestone_name):
+        """
+        Returns True if the milestone exists.
+        """
+        if not self._milestones:
+            self._milestones = []
+            for _milestone in self._repo.get_milestones(state="all"):
+                m = Milestone(self._repo, _milestone)
+                self._milestones.append(m)
+        found = False
+        for m in self._milestones:
+            if m._milestone.title == milestone_name:
+                found = True
+        return found
+
+    def get_milestone(self, milestone_name):
+        """
+        Returns the milestone with the name (or None)
+        """
+        # force lazy evaluation
+        if self.milestone_exists(milestone_name):
+            for m in self._milestones:
+                if m._milestone.title == milestone_name:
+                    return m
+        return None
+
+    def create_milestone(self, milestone_name, date=None):
+        """
+        If the milestone does not exist, create it.
+
+        If (optional) date passed in, set that date on the
+        milestone.
+        """
+        # FIXME: validate date input (if not None)
+        # accept strings of dates?
         pass
 
 
--- src/tests/test_gh.py
+++ src/tests/test_gh.py
@@ -7,7 +7,8 @@ The testing strategy for the IP validator is:
 """
 import unittest
 import json
-from mocks import mock_get
+from unittest.mock import MagicMock
+from mocks import mock_get, MockCallback
 
 # hackery
 import sys
@@ -21,7 +22,6 @@ sys.path.append(
 # patchery
 import gh
 gh.requests.get = mock_get
-
 
 # actual test code...
 BAD_MOCK_ADDRESSES = ('193.21.243.7', '185.198.107.43')
@@ -46,6 +46,174 @@ class IPValidationTestCase(unittest.TestCase):
         for addr in BAD_MOCK_ADDRESSES:
             validity = val.ip_str_is_valid(addr)
             self.assertFalse(validity)
+
+
+class RepoTestCase(unittest.TestCase):
+    def test_init(self):
+        cb = MockCallback()
+        payload = cb.payload()
+        # we require construction without exception
+        try:
+            repo = gh.Repo(payload['repository']['full_name'])
+            was_error = False
+        except:
+            was_error = True
+        self.assertFalse(was_error)
+
+
+class RepoEnsureMilestoneExistsTestCase(unittest.TestCase):
+    def test_does_nothig_if_it_exists(self):
+        """
+        Given the milestone exists
+        When I call repo.ensure_milestone_exists
+        Then no new milestone is created
+        """
+        repo = gh.repo_from_callback(MockCallback())
+        # if the milestone exists
+        repo.milestone_exists = MagicMock(return_value=True)
+        # it should not try to create it
+        repo.create_milestone = MagicMock()
+        repo.ensure_milestone_exists('foo')
+        repo.create_milestone.assert_not_called()
+
+    def test_creates_milestone_if_not_exist(self):
+        """
+        Given the milestone does not exist
+        When I call repo.ensure_milestone_exists
+        Then a new milestone is created
+        """
+        repo = gh.repo_from_callback(MockCallback())
+        # if the milestone does not exist
+        repo.milestone_exists = MagicMock(return_value=False)
+        # it should be created
+        repo.create_milestone = MagicMock()
+        repo.ensure_milestone_exists('foo')
+        repo.create_milestone.assert_called()
+
+
+class RepoEnsureMilestoneHasDueDateTestCase(unittest.TestCase):
+    def test_set_date_if_none(self):
+        """
+        Given a milestone has no due date
+        When I call repo.ensure_milestone_has_due_date
+        Then the due date is updated as specified
+        """
+        DATE =  '2012-10-09T23:39:01Z'
+        repo = gh.repo_from_callback(MockCallback())
+        # if the milestone exists
+        repo.milestone_exists = MagicMock(return_value=True)
+        # and has no due date
+        mm = gh.Milestone('some/repo', '_milestone')
+        mm.get_due_date = MagicMock(return_value=None)
+        mm.set_due_date = MagicMock()
+        repo.get_milestone = MagicMock(return_value=mm)
+        # then ensure the date is set
+        repo.ensure_milestone_has_due_date('foo', DATE)
+        # was the date updated right?
+        mm.set_due_date.assert_called_with(DATE)
+
+    def test_do_nothing_if_date_OK(self):
+        """
+        Given a malestone already has a specified due date
+        When I call repo.ensure_milestone_has_due_date
+        Then the due date is not updated
+        """
+        DATE =  '2012-10-09T23:39:01Z'
+        repo = gh.repo_from_callback(MockCallback())
+        # if the milestone exists
+        repo.milestone_exists = MagicMock(return_value=True)
+        # and has no due date
+        mm = gh.Milestone('some/repo', '_milestone')
+        mm.get_due_date = MagicMock(return_value=DATE)
+        mm.set_due_date = MagicMock()
+        repo.get_milestone = MagicMock(return_value=mm)
+        # then ensure the date is set
+        repo.ensure_milestone_has_due_date('foo', DATE)
+        # was the date updated right?
+        mm.set_due_date.assert_not_called()
+
+    def test_update_if_wrong_date(self):
+        """
+        Given a milestone exists with a due date
+        And the due date is not the specified due date
+        When I call repo.ensure_milestone_has_due_date
+        Then the due date is updated
+        """
+        DATE =  '2012-10-09T23:39:01Z'
+        WRONG_DATE = '2030-10-09T23:39:01Z'
+        repo = gh.repo_from_callback(MockCallback())
+        # if the milestone exists
+        repo.milestone_exists = MagicMock(return_value=True)
+        # and has no due date
+        mm = gh.Milestone('some/repo', '_milestone')
+        mm.get_due_date = MagicMock(return_value=WRONG_DATE)
+        mm.set_due_date = MagicMock()
+        repo.get_milestone = MagicMock(return_value=mm)
+        # then ensure the date is set
+        repo.ensure_milestone_has_due_date('foo', DATE)
+        # was the date updated right?
+        mm.set_due_date.assert_called_with(DATE)
+
+
+class RepoMilestoneExistsTestCase(unittest.TestCase):
+    def test_true_if_milestone_exists(self):
+        """
+        Given a milestone exists
+        When I call repo.milestone_exists
+        Then True is returned
+        """
+        # givem a milestone exists
+        MILESTONE_NAME = "foo"
+        repo = gh.repo_from_callback(MockCallback())
+        class MockMilestoneWrapper:
+            def __init__(self):
+                self._milestone = MockMilestone()
+        class MockMilestone:
+            def __init__(self):
+                self.title = MILESTONE_NAME
+        repo._milestones = (MockMilestoneWrapper(),)
+        # then milestone exists
+        self.assertTrue(repo.milestone_exists(MILESTONE_NAME))
+
+
+    def test_false_if_milestone_not_exists(self):
+        """
+        Given a milestone does not exist
+        When I call repo.milestone_exists
+        Then False is returned
+        """
+        # givem a milestone exists
+        MILESTONE_NAME = "foo"
+        OTHER_MILESTONE_NAME = "bar"
+        repo = gh.repo_from_callback(MockCallback())
+        class MockMilestoneWrapper:
+            def __init__(self):
+                self._milestone = MockMilestone()
+        class MockMilestone:
+            def __init__(self):
+                # this is not the milestone you are looking for
+                self.title = OTHER_MILESTONE_NAME
+        repo._milestones = (MockMilestoneWrapper(),)
+        # then the milestone does not exist
+        self.assertFalse(repo.milestone_exists(MILESTONE_NAME))
+
+
+class RepoGetMilestoneTestCase(unittest.TestCase):
+    def test_returns_non_if_not_exists(self):
+        pass  # FIXME
+    def test_returns_milestone_if_exists(self):
+        pass  # FIXME
+
+
+class RepoCreateMilestoneTestCase(unittest.TestCase):
+    def test_creates_if_not_exist(self):
+        pass  # FIXME
+    def test_error_if_already_exists(self):
+        pass  # FIXME
+    def test_date_none_if_none_provided(self):
+        pass  # FIXME
+    def test_date_matches_supplied(self):
+        pass  # FIXME
 
 
 if __name__ == '__main__':
